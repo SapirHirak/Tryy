fb51e9ac3a57f4bc1e7fbb0b1f0118a8d431d957
SapirHirak <sapiroosh1h@gmail.com>
2020-10-16 17:06:13 +0300

    first

---
+++ stam.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+typedef int number;
+int main()
+{
+    number employee;
+    std::cout << "Hello, World!" << std::endl;
+    std::cin >> employee;
+    std::cout << "Number Of employees: " << employee;
+
+    return 0;
+}
